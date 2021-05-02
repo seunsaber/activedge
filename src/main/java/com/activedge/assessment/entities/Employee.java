cf1d092c0c72934b2ac27cca7928a1350f713ac2
Seun Fapohunda <seunfapps@gmail.com>
2021-05-02 01:23:44 +0100

    Employee Entity

---
+++ src/main/java/com/activedge/assessment/entities/Employee.java
@@ -0,0 +1,51 @@
+package com.activedge.assessment.entities;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private LocalDate joinDate;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(LocalDate joinDate) {
+        this.joinDate = joinDate;
+    }
+}
